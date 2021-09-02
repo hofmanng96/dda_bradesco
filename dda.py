4f4a08990768a1e1f7f89c8276662f99e66dac3d
hofmanng96 <hofmannguilherme@gmail.com>
2021-09-02 14:31:12 -0300

    Add files via upload

---
+++ dda.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import fileinput
+import glob
+import sys
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("496599520000015", "000049659952015"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("176513000158000", "000176513000158"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("959473100000015", "000095947310015"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("017520160000092", "000001752016092"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("576045500000010", "000005760455001"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("576045500000010", "000005760455001"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("255725550000053", "000025572555053"))
+
+for line in fileinput.input(glob.glob('C:\Skyline\inbox\PAG_*.ret'), inplace=True):
+    sys.stdout.write(line.replace("561608870000068", "000056160887068"))
